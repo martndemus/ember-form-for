baad49468c591d7a4dd875c2a6273eb737f4626d
Michael Padon <maprules1000@gmail.com>
2016-09-14 11:18:52 -0500

    adds submit default classes on init

--- addon/components/form-controls/submit.js
+++ addon/components/form-controls/submit.js
@@ -1,8 +1,19 @@
+import Ember from 'ember';
 import ButtonComponent from './button';
 import { invokeAction } from 'ember-invoke-action';
 
+const { get, set } = Ember;
+
 export default ButtonComponent.extend({
   type: 'submit',
+
+  init() {
+    this._super(...arguments);
+
+    let submitClasses = get(this, 'config.submitClasses');
+    let classNames = get(this, 'classNames');
+    set(this, 'classNames', (classNames || []).concat(submitClasses));
+  },
 
   click(e) {
     e.preventDefault();
