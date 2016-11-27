bd1591ea7cf83bcf0f1241893438cc128912f40c
Manuel Wiedenmann <manuel@funkensturm.de>
2016-11-27 15:35:08 +0100

    Make it backwards compatible

--- addon/components/form-errors.js
+++ addon/components/form-errors.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 import layout from '../templates/components/form-errors';
 
-const { computed, get } = Ember;
+const {
+  computed,
+  computed: { notEmpty },
+  get
+} = Ember;
 
 export default Ember.Component.extend({
   layout,
   tagName: '',
+
+  showErrors: notEmpty('errors'),
 
   joinedErrorClasses: computed('errorClasses', function() {
     return (get(this, 'errorClasses') || []).join(' ');
